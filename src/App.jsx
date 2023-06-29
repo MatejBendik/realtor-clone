2d19af3271cebae15a1d30244cc5138ba93342f8
MatejBendik <matejbendik.mb@gmail.com>
2023-06-29 21:29:52 +0200

    Add React router with all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1 className="text-center text-red-400 text-6xl">Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
